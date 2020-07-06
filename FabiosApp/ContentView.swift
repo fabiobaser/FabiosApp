20d418eb57c6babba9a1d139189221ffffeef0cb
Fabio Baser <git@fabiobaser.de>
2020-07-06 19:20:54 +0200

    Initial Commit

---
+++ FabiosApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FabiosApp
+//
+//  Created by Fabio Baser on 06.07.20.
+//  Copyright © 2020 Fabio Baser. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
